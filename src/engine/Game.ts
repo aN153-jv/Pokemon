cfb58792cafc45d51eae745ea24f1067c5a546de
aN153-jv <axellnicol@outlook.fr>
2026-07-22 14:40:46 +0200

    Update Game.ts

--- src/engine/Game.ts
+++ src/engine/Game.ts
@@ -1,1 +1,11 @@
-// TODO
+import Phaser from "phaser";
+import Config from "./Config";
+
+export default class Game extends Phaser.Game {
+  constructor(parent: string) {
+    super({
+      ...Config,
+      parent,
+    });
+  }
+}
