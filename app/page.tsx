7c21816359ac720948ae7f7c15c42355774b4d86
aN153-jv <axellnicol@outlook.fr>
2026-07-22 14:40:10 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,16 @@
-// TODO
+"use client";
+
+import { useEffect } from "react";
+import Game from "@/src/engine/Game";
+
+export default function Home() {
+  useEffect(() => {
+    const game = new Game("game-container");
+
+    return () => {
+      game.destroy(true);
+    };
+  }, []);
+
+  return <div id="game-container" />;
+}
