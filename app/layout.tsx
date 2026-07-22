c45be79335855cdbe554ed507bad0bab84cb0633
aN153-jv <axellnicol@outlook.fr>
2026-07-22 14:39:38 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,18 @@
-// TODO
+import "./globals.css";
+
+export const metadata = {
+  title: "Pokemon Game",
+  description: "Jeu style Pokémon"
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="fr">
+      <body>{children}</body>
+    </html>
+  );
+}
