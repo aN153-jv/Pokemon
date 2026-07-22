6d4554482813536439eb9018b0582f10face5d57
aN153-jv <axellnicol@outlook.fr>
2026-07-22 14:40:31 +0200

    Update Config.ts

--- src/engine/Config.ts
+++ src/engine/Config.ts
@@ -1,1 +1,28 @@
-// TODO
+import Phaser from "phaser";
+
+import Boot from "@/src/scenes/Boot";
+import Preload from "@/src/scenes/Preload";
+import World from "@/src/scenes/World";
+
+const Config: Phaser.Types.Core.GameConfig = {
+  type: Phaser.AUTO,
+  width: 1920,
+  height: 1080,
+  parent: "game-container",
+  backgroundColor: "#000000",
+  pixelArt: true,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { x: 0, y: 0 },
+      debug: false,
+    },
+  },
+  scene: [Boot, Preload, World],
+};
+
+export default Config;
