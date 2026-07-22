e7f83814d037ed8f9c99d4f8ca535fa513bb58af
aN153-jv <axellnicol@outlook.fr>
2026-07-22 15:07:17 +0200

    Update BattleUI.ts

--- src/battle/BattleUI.ts
+++ src/battle/BattleUI.ts
@@ -1,1 +1,9 @@
-// TODO
+// Exemple de structure pour votre affichage de combat
+export function renderBattleUI(player: any, enemy: any, onAttack: (index: number) => void) {
+  return `
+    <div class="battle-arena">
+      <div class="enemy-stats">${enemy.name} - HP: ${enemy.hp}/${enemy.maxHp}</div>
+      <div class="player-stats">${player.name} - HP: ${player.hp}/${player.maxHp}</div>
+    </div>
+  `;
+}
