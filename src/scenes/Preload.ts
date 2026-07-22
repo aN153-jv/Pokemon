7991f5aae725b0f998d269b36e61b4f6e8f49ff0
aN153-jv <axellnicol@outlook.fr>
2026-07-22 14:47:09 +0200

    Update Preload.ts

--- src/scenes/Preload.ts
+++ src/scenes/Preload.ts
@@ -1,1 +1,21 @@
-// TODO
+import Phaser from "phaser";
+
+export default class Preload extends Phaser.Scene {
+  constructor() {
+    super("Preload");
+  }
+
+  preload() {
+    const graphics = this.add.graphics();
+
+    graphics.fillStyle(0x3b82f6);
+    graphics.fillRect(0, 0, 32, 32);
+
+    graphics.generateTexture("player", 32, 32);
+    graphics.destroy();
+  }
+
+  create() {
+    this.scene.start("World");
+  }
+}
