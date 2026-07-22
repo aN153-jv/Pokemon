b5850a3b01f39631f21087ca85eb9881d53c0353
aN153-jv <axellnicol@outlook.fr>
2026-07-22 14:10:20 +0200

    Create boot.ts

---
+++ src/game/scenes/boot.ts
@@ -0,0 +1,17 @@
+import Phaser from "phaser";
+
+export default class Boot extends Phaser.Scene{
+
+    constructor(){
+
+        super("Boot");
+
+    }
+
+    create(){
+
+        this.scene.start("Preload");
+
+    }
+
+}
