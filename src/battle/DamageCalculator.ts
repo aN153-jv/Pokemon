60c3d3529f2fab988692562976f3a9e4519330d8
aN153-jv <axellnicol@outlook.fr>
2026-07-22 15:07:30 +0200

    Update DamageCalculator.ts

--- src/battle/DamageCalculator.ts
+++ src/battle/DamageCalculator.ts
@@ -1,1 +1,5 @@
-// TODO
+export function calculateDamage(attacker: any, defender: any, move: any): number {
+  // Formule de dégâts basique (ou inspirée de Pokémon)
+  const baseDamage = move.power;
+  return Math.max(1, baseDamage);
+}
